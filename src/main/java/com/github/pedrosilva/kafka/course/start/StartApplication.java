9efc8c943d86a062d872e09c196cd18b7b7cccf0
Pedro Silva <pedro.silva@keyrus.com.br>
2019-10-04 17:33:44 -0300

    dependencies set and main class start

---
+++ src/main/java/com/github/pedrosilva/kafka/course/start/StartApplication.java
@@ -0,0 +1,8 @@
+package com.github.pedrosilva.kafka.course.start;
+
+public class StartApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Beginning.");
+    }
+}
