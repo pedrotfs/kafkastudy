efdc9c40f45e94f8149f277c1a9d3f2261cf3abf
Pedro Silva <pedro.silva@keyrus.com.br>
2019-10-08 16:49:30 -0300

    kfk11 full connection twitter elastic

--- ElasticReader/src/main/java/com/github/pedrosilva/kafka/course/elastic/reader/start/StartElasticConsumer.java
+++ ElasticReader/src/main/java/com/github/pedrosilva/kafka/course/elastic/reader/start/StartElasticConsumer.java
@@ -1,6 +1,11 @@
 package com.github.pedrosilva.kafka.course.elastic.reader.start;
 
+import com.github.pedrosilva.kafka.course.consumer.KafkaConsumerFactory;
 import com.github.pedrosilva.kafka.course.elastic.reader.utils.ElasticSearchClientFactory;
+import com.github.pedrosilva.kafka.course.util.KafkaCourseUtils;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -10,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class StartElasticConsumer {
 
@@ -18,6 +24,10 @@ public class StartElasticConsumer {
     private static final String INDEX = "twitter";
 
     private static final String INDEX_TYPE = "tweets";
+
+    private static final String CONSUMER_GROUP = "elasticsearch-tweets";
+
+    private static final String CONSUMER_TOPIC = "tweets";
 
     private StartElasticConsumer() {
         //OK
@@ -31,17 +41,39 @@ public class StartElasticConsumer {
     {
         try {
             RestHighLevelClient client = ElasticSearchClientFactory.createClient();
+            KafkaConsumerFactory consumerFactory = new KafkaConsumerFactory();
 
-            final String json = "{\"property\":\"value\"}";
+            final KafkaConsumer<String, String> consumer = consumerFactory.createConsumer(CONSUMER_GROUP,
+                    KafkaCourseUtils.DEFAULT_BOOTSTRAP_SERVER_VALUE, KafkaCourseUtils.DEFAULT_BOOTSTRAP_SERVER_PORT, CONSUMER_TOPIC);
 
-            IndexRequest indexRequest = new IndexRequest(INDEX, INDEX_TYPE).source(json, XContentType.JSON);
-            IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-
-            LOG.info(indexResponse.getId());
+            consumeKafkaFeed(consumer, client);
+            consumer.close();
             client.close();
 
         } catch (IOException e) {
             LOG.error("can't connect to elastic search cloud.");
+        }
+    }
+
+    private void consumeKafkaFeed(KafkaConsumer<String, String> consumer, RestHighLevelClient client) {
+        while(true){
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
+            records.forEach(r -> {
+                LOG.info("Key: " + r.key() + " - Val: " + r.value());
+                LOG.info("Prt: " + r.partition() + " - Tms: " + r.timestamp() + " - Off: " + r.offset());
+
+                IndexRequest indexRequest = new IndexRequest(INDEX, INDEX_TYPE).source(r.value(), XContentType.JSON);
+                IndexResponse indexResponse = null;
+                try {
+                    indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                    LOG.info("response id : " + indexResponse.getId());
+                } catch (IOException e) {
+                    LOG.error("why cant I just throw this?");
+                } catch (ElasticsearchException e)
+                {
+                    LOG.info("Key: " + r.key() + " - Val: " + r.value() + "has given an exception indexing!!!!!!!!!!!");
+                }
+            });
         }
     }
 }
